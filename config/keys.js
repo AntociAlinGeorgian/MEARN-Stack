f3d3b96df060ffed2ad7680a4277df3882438d0b
Alin Georgian, Antoci <brinza.alin.georgian@ro.ibm.com>
2018-07-05 15:43:13 +0300

    Creating the JWT

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb://alin:alin123@ds227481.mlab.com:27481/mearn-socialnetwork"
+  mongoURI:
+    "mongodb://alin:alin123@ds227481.mlab.com:27481/mearn-socialnetwork",
+  secretOrKey: "secret"
 };
