77b7c8dd993515e248675692b09ea57a5382ba31
Alin Georgian, Antoci <brinza.alin.georgian@ro.ibm.com>
2018-07-05 16:07:09 +0300

    Passport JWT authentication strategy

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
+const passport = require("passport");
 
 //load user model
 const User = require("../../models/User");
@@ -73,24 +74,6 @@ router.post("/login", (req, res) => {
     //check password
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
-        // //user matched
-
-        // //create jwt payload
-        // const payload = {
-        //   id: user.id,
-        //   name: user.name,
-        //   avatar: user.avatar
-        // };
-
-        // //sign token
-        // jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, () => {
-        //   res.json({
-        //     success: true,
-        //     token: "Bearer " + token
-        //   });
-        // });
-        // return res.status(200).json({ password: "Success!" });
-
         // User Matched
         const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT Payload
 
@@ -112,5 +95,22 @@ router.post("/login", (req, res) => {
     });
   });
 });
+
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    //res.json({ msg: "Success!" });
+    //res.json(req.user);
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
